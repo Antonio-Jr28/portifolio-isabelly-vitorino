8dec88627d961032f411f274a30baef3c5876555
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-11-28 23:53:38 -0300

    criando hero, adicionando background

--- src/components/hero/hero.component.tsx
+++ src/components/hero/hero.component.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export const Hero = () => {
   return (
-    <div className="flex items-center justify-around bg-[#592f16]">
-      <img
-        src="image/isabelly-vitorino.png"
-        alt="Descrição da imagem"
-        className="w-64 h-64 object-contain"
-      />
-      <div className="flex flex-col md:items-end">
-        <h1 className="text-4xl text-[#d9b29c]">Isabelly Vitorino</h1>
-        <p className="text-xl sm:ml-32px text-[#d9b29c]">Jornalista</p>
+    <div
+      className="w-full h-[250px] bg-cover bg-bottom"
+      style={{
+        backgroundImage: "url('image/background.jpg')",
+      }}
+    >
+      <div className="flex flex-col md:items-center">
+        <h1 className="pt-20 text-5xl text-[#592F16]">Isabelly Vitorino</h1>
+        <p className="text-2xl sm:ml-32px text-[#592F16]">Jornalista</p>
       </div>
     </div>
   );
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { Navbar } from "../components/navbar/navbar.component";
+import { Hero } from "../components/hero/hero.component";
 
 export const HomePage = () => {
-    return (
-        <>
-        <Navbar />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+};
