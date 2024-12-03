61f144a61cfa54e485bb31a972e553842c35ec34
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-12-03 16:48:06 -0300

    adicionando descricao na sessao trabalho

--- src/components/works/works-all.component.tsx
+++ src/components/works/works-all.component.tsx
@@ -9,35 +9,38 @@ import { news } from "../../data/newsData";
 
 export const WorksAll = () => {
   return (
-    <div className="w-auto h-auto p-6 bg-gray-100">
-      <Swiper
-        modules={[Navigation, Autoplay]}
-        pagination={{ clickable: true }}
-        navigation
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true,
-        }}
-        loop={true}
-        spaceBetween={20}
-        slidesPerView={1}
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
-        }}
-      >
-        {news.map((item, index) => (
-          <SwiperSlide key={index}>
-            <NewsCard
-              imageSrc={item.imageSrc}
-              title={item.title}
-              content={item.content}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <>
+      <div className="w-auto h-auto p-6 bg-gray-100">
+        <h1 className="text-4xl text-center py-6">Trabalhos Recentes</h1>
+        <Swiper
+          modules={[Navigation, Autoplay]}
+          pagination={{ clickable: true }}
+          navigation
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }}
+          loop={true}
+          spaceBetween={20}
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {news.map((item, index) => (
+            <SwiperSlide key={index}>
+              <NewsCard
+                imageSrc={item.imageSrc}
+                title={item.title}
+                content={item.content}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
   );
 };
