51d7d2d3bda8f0a25d6e4b93f4f9c328499596a6
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-12-03 18:05:20 -0300

    criando pagina works

---
+++ src/pages/works.poge.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Navbar } from "../components/navbar/navbar.component";
+import { Hero } from "../components/hero/hero.component";
+
+export const WorksPage = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "../pages/home.page";
 import { AboutUsPage } from "../pages/about-us.page";
+import { WorksPage } from "../pages/works.poge";
 
 export const MyRoutes = () => {
   return (
@@ -9,7 +10,7 @@ export const MyRoutes = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/sobre" element={<AboutUsPage />} />
-        <Route path="/trabalhos" element="/trabalhos" />
+        <Route path="/trabalhos" element={<WorksPage />} />
         <Route path="/contato" element="/contato" />
       </Routes>
     </BrowserRouter>
