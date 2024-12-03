36e00665db45f1288cc19e163cfce6062c0ce5cd
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-12-03 14:43:22 -0300

    ajustando alinhamento do card

--- src/components/works/news-card-works.component.tsx
+++ src/components/works/news-card-works.component.tsx
@@ -15,7 +15,7 @@ export const NewsCard: React.FC<NewsCardProps> = ({
     <div className="flex flex-col items-center p-6 bg-[#A66329] border border-[#F2C879]">
       <img src={imageSrc} alt={title} className="w-[500px] h-[150px]" />
       <h1 className="text-2xl text-center text-white">{title}</h1>
-      <p className="w-[420px] text-center p-2">{content}</p>
+      <p className="w-auto text-center p-2">{content}</p>
       <button className="w-52 border text-center">Ler mais</button>
     </div>
   );
