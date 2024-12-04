32bc2bbd788b177749cc235537c436f97ff1740c
Antonio-Jr28 <goncalvesantonio.jr@gmail.com>
2024-12-03 23:41:14 -0300

    editando cores dos textos

--- src/components/works/news-card-works.component.tsx
+++ src/components/works/news-card-works.component.tsx
@@ -17,9 +17,9 @@ export const NewsCard: React.FC<NewsCardProps> = ({
     <div className="p-6 bg-[#A66329] border border-[#F2C879]">
       <div className="flex flex-col items-start">
         <img src={imageSrc} alt={title} className="w-[500px] h-[150px]" />
-        <label>{label}</label>
+        <label className="text-white">{label}</label>
         <h1 className="text-2xl text-white">{title}</h1>
-        <p className="w-auto ">{content}</p>
+        <p className="w-auto text-white">{content}</p>
       </div>
       <div className="flex flex-col items-center">
         <button className="w-52 border-2 mt-3 border-white bg-[#f2c879] hover:bg-[#bf7d2c] ">
